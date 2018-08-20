a277fd2a005af220be8d1cf98c5c1a4715858467
Junxiang <junxiang.oriint@gmail.com>
2018-08-20 15:22:26 +0800

    wip

--- Back-End/Admin/config/prod.env.js
+++ Back-End/Admin/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://wg-mediatech.azurewebsites.net//api"',  
+  BASE_API: '"https://wg-mediatech.azurewebsites.net/api"',  
   // BASE_API: '"https://www.media-tech.com.tw/api"',  
 }
